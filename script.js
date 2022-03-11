b024987efdd2381030c4724fd08dd9f41b800038
Romulo <romulosilva001@gmail.com>
2022-03-11 16:06:01 -0300

    Adicionado resete do campo que recebe a URL no script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let urlVideos = [];
+
 
 function converterUrl(urlWatch) {
   urlWatch = urlWatch.split("watch?v=");
@@ -8,14 +8,16 @@ function converterUrl(urlWatch) {
 
 
 
+let urlVideos = []; /*Variável tem que ficar fora da função para não resetar os valores recebidos*/
+
 function adicionar() {
   let div = document.querySelector("#div-videos");
   div.style.display = "flex";
   
-  let url = document.querySelector("#url");
-
-  let urlPronta = converterUrl(url.value);
-
+  let url = document.querySelector("#url").value;
+  let urlPronta = converterUrl(url);
+  document.querySelector("#url").value = "";/*Resetando o campo que recebe a URL*/
+  
   urlVideos.push(`<iframe src="${urlPronta}"></iframe>`);
   div.innerHTML = urlVideos;
 }
